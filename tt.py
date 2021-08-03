9567e3d7d4e58c5d1a3195d704ceb3044a595a17
kojo120 <77032249+kojo120@users.noreply.github.com>
2021-08-02 21:30:07 -0400

    Create tt.py

---
+++ tt.py
@@ -0,0 +1,18 @@
+import csv
+from csv import DictReader
+
+with open('input.csv', 'r') as csv_file: 
+    csv_reader = DictReader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+       #print(row [line_count]) 
+        print(row)
+    for row in csv_reader:
+       #print(row [line_count]) 
+        print(row)
+        
+        
+        
+        
+        
+        https://www.dezyre.com/recipes/perform-data-validation-python-by-processing-only-matched-columns
